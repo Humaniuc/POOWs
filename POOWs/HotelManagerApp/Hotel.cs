5f9854e3efd7c2632c3254ba6ea284dffd6ef942
Humaniuc <humancoria@gmail.com>
2019-05-12 19:01:12 +0300

    HotelClass

---
+++ POOWs/HotelManagerApp/Hotel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace HotelManagerApp
+{
+    class Hotel
+    {
+        private string name;
+        private string city;
+        private List<Room> rooms = new List<Room>();
+        internal Hotel(string name, string city)
+        {
+            this.name = name;
+            this.city = city;
+        }
+        internal string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        internal string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        internal List<Room> Rooms
+        {
+            get { return rooms; }
+            set { rooms = value; }
+        }
+
+        internal void Print()
+        {
+            System.Console.WriteLine($"{name} from {city} has: ");
+            foreach(Room r in rooms)
+            {
+                r.Print();
+            }
+        }
+    }
+}
--- POOWs/HotelManagerApp/Program.cs
+++ POOWs/HotelManagerApp/Program.cs
@@ -4,12 +4,23 @@ using System.Threading.Tasks;
 
 namespace HotelManagerApp
 {
-    class Program
+    partial class Program
     {
         static void Main()
         {
-            
+            Hotel h1 = new Hotel("Hotel 1", "Iasi");
+            Rate rate = new Rate(15.6, "Euro");
+            Room r1 = new Room(1, rate);
+            r1.AdultsNumber = 2;
+            r1.ChildrenNumber = 1;
+            h1.Rooms.Add(r1);
+            rate.Amount = 18;
+            Room r2 = new Room(2, rate);
+            r2.AdultsNumber = 2;
+            r2.ChildrenNumber = 2;
+            h1.Rooms.Add(r2);
 
+            h1.Print();
         }
     }
 }
