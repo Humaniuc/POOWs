7ae22ec3b87c6f2b82cca298682e75453b8f0529
Humaniuc <humancoria@gmail.com>
2019-05-12 15:00:03 +0300

    RateClass

---
+++ POOWs/HotelManagerApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagerApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Rate r = new Rate(500, "Ron");
+
+            r.Print();
+        }
+    }
+}
---
+++ POOWs/HotelManagerApp/Rate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelManagerApp
+{
+    public class Rate
+    {
+        readonly List<string> currency = new List<string>() { "RON", "EURO", "DOLLAR" };
+        internal double Amount
+        {
+            get
+            {
+                return Amount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.Amount = value;
+                }
+                else
+                {
+                    Console.WriteLine("The price cannot be negative.");
+                }
+            }
+        }
+
+        internal string Currency
+        {
+            get
+            {
+                return Currency;
+            }
+            set
+            {
+                if(currency.Contains(value.ToUpper()))
+                {
+                    Currency = value.ToUpper();
+                }
+            }
+        }
+
+        internal Rate(double amount, string currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+        internal void Print()
+        {
+            Console.WriteLine($"Amount: {Amount}{Currency}");
+        }
+    }
+}
