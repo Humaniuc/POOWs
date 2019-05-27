07a6e418b1cf471f7eeda1c52ab70b2cd0de7882
Humaniuc <humancoria@gmail.com>
2019-05-27 23:02:50 +0300

    Nullable Int App

---
+++ Week6WantsomeHomework/PersonApp/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace PersonApp
+{
+    class Person
+    {
+        private readonly string name;
+        private int? age;
+
+        internal string Name { get; private set; }
+        internal int? Age { get; set; }
+
+        internal Person(string name)
+        {
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + "\nAge: " + (Age == null ? "Age isn't set" : $"{Age}");
+        }
+    }
+}
---
+++ Week6WantsomeHomework/PersonApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pers = new Person("Humaniuc");
+            Console.WriteLine(pers.ToString());
+
+            pers.Age = 30;
+            Console.WriteLine(pers.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
