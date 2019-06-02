b6da47500acb7f4d42eb12fc8b4fe1abcb9a205e
Humaniuc <humancoria@gmail.com>
2019-06-02 22:35:24 +0300

    Download a fiel from internet

---
+++ Week7aWantsomeHomework/DownloadAFileFromNet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace DownloadAFileFromNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("https://www.desicomments.com/wp-content/uploads/Samurai-Jack-Doing-Practise-Dc36.gif", "SamuraiJack.gif");
+            }
+            catch(ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(WebException e)
+            {
+                Console.WriteLine(e.InnerException);
+            }
+            catch(NotSupportedException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
