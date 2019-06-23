0e0510deee6fb0a95ea9e9adb77be5f819fbfcb8
Humaniuc <humancoria@gmail.com>
2019-06-23 15:12:14 +0300

    ActionDelegates

---
+++ POOWs/Week7BHomeworkWantsome/ActionDelegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionDelegates
+{
+    class Program
+    {
+        static void ConsolePrint(int i)
+        {
+            Console.WriteLine(i);
+        }
+        static void Main(string[] args)
+        {
+            Action<int> printActionDel = ConsolePrint;
+            printActionDel(10);
+
+            Action<int> printActionAnonym = delegate (int i)
+            {
+                Console.WriteLine(i);
+            };
+            printActionAnonym(11);
+
+            Action<int> printActionLambda = i => Console.WriteLine(i);
+            printActionLambda(12);
+        }
+    }
+}
