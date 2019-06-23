0b9fe2371c093b28dfca536fe7e7547ecb4bfd2b
Humaniuc <humancoria@gmail.com>
2019-06-23 15:05:47 +0300

    FuncDelegates

---
+++ POOWs/Week7BHomeworkWantsome/FuncDelegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncDelegates
+{
+    class Program
+    {
+        public delegate int SomeOperation(int i, int j);
+
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            SomeOperation add = Sum;
+            int result = add(10, 10);
+            Console.WriteLine(result);
+
+            Func<int, int, int> addFunc = Sum;
+            result = addFunc(10, 10);
+            Console.WriteLine(result);
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random rnd = new Random();
+                return rnd.Next(1, 100);
+            };
+            Console.WriteLine("Random number: " + getRandomNumber());
+
+            Func<int> getRndNumber = () => new Random().Next(1, 100);
+            Console.WriteLine("Random number: " + getRndNumber());
+
+            Func<int, int, int> Suma = (x, y) => x + y;
+            Console.WriteLine("Sum = " + Suma(12, 23));
+        }
+    }
+}
--- POOWs/Week7BHomeworkWantsome/Week7BHomeworkWantsome/Program.cs
+++ POOWs/Week7BHomeworkWantsome/Week7BHomeworkWantsome/Program.cs
@@ -6,47 +6,50 @@ using System.Threading.Tasks;
 
 namespace Week7BHomeworkWantsome
 {
-    class Program
+    namespace CustomDelegates
     {
-        public delegate void Print(int value);
-        static void Main(string[] args)
+        class Program
         {
-            Print printDel = PrintNumber;
-            printDel(100000);
-            printDel(200);
+            public delegate void Print(int value);
+            static void Main(string[] args)
+            {
+                Print printDel = PrintNumber;
+                printDel(100000);
+                printDel(200);
 
-            printDel = PrintMoney;
-            printDel(100000);
-            printDel(200);
+                printDel = PrintMoney;
+                printDel(100000);
+                printDel(200);
 
-            PrintHelper(PrintNumber, 10000);
-            PrintHelper(PrintMoney, 10000);
+                PrintHelper(PrintNumber, 10000);
+                PrintHelper(PrintMoney, 10000);
 
-            printDel = PrintNumber;
-            printDel += PrintMoney;
-            printDel += PrintHexazecimal;
-            printDel(1000);
+                printDel = PrintNumber;
+                printDel += PrintMoney;
+                printDel += PrintHexazecimal;
+                printDel(1000);
 
-            printDel -= PrintHexazecimal;
-            printDel(2000);
-        }
+                printDel -= PrintHexazecimal;
+                printDel(2000);
+            }
 
-        public static void PrintNumber(int num)
-        {
-            Console.WriteLine("Number: {0,-12:N0}", num);
-        }
-        public static void PrintMoney(int money)
-        {
-            Console.WriteLine("Money: {0:C}", money);
-        }
+            public static void PrintNumber(int num)
+            {
+                Console.WriteLine("Number: {0,-12:N0}", num);
+            }
+            public static void PrintMoney(int money)
+            {
+                Console.WriteLine("Money: {0:C}", money);
+            }
 
-        public static void PrintHexazecimal(int dec)
-        {
-            Console.WriteLine("Hexazecimal: {0:X}", dec);
-        }
-        public static void PrintHelper(Print delegateFunc, int numToPrint)
-        {
-            delegateFunc(numToPrint);
+            public static void PrintHexazecimal(int dec)
+            {
+                Console.WriteLine("Hexazecimal: {0:X}", dec);
+            }
+            public static void PrintHelper(Print delegateFunc, int numToPrint)
+            {
+                delegateFunc(numToPrint);
+            }
         }
     }
 }
