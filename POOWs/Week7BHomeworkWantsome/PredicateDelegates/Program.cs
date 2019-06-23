ddc6c0a975c0dbecf0e7cd0fd6767353510b6bea
Humaniuc <humancoria@gmail.com>
2019-06-23 15:25:25 +0300

    PredicateDelegates

---
+++ POOWs/Week7BHomeworkWantsome/PredicateDelegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateDelegates
+{
+    class Program
+    {
+        static bool IsUpperCase(string str)
+        {
+            return str.Equals(str.ToUpper());
+        }
+        static void Main(string[] args)
+        {
+            // Predicate delegate
+            Predicate<string> isUpper = IsUpperCase;
+            string s = "hello world!!";
+            bool result = isUpper(s);
+            Console.WriteLine("{0} isUpper? : {1}", s, result);
+
+            // Predicate delegate with anonymous method
+            Predicate<string> isUpperAnonym = delegate (string str) { return str.Equals(str.ToUpper()); };
+            Console.WriteLine("{0} isUpper? = {1}", s, isUpperAnonym(s));
+
+            // Predicate delegate with lambda expression
+            Predicate<string> isUpperLambda = sLam => sLam.Equals(sLam.ToUpper());
+            Console.WriteLine("{0} isUpper? = {1}", s, isUpperLambda(s));
+
+        }
+    }
+}
