31185e8fad7aa118dc7f90b8d66ae238b0b82526
Humaniuc <humancoria@gmail.com>
2019-06-23 13:21:06 +0300

    PrintDelegate

---
+++ Week7aWantsomeHomework/BitArray64/BitArray64.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace BitArray64
+{
+    class BitArray64 : IEnumerable<int>
+    {
+        private ulong num;
+
+        public BitArray64()
+        {
+            num = 0b00000000000000000000000000000000;
+        }
+        public ulong Num { get; set; }
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return (int) num | (0b1 << 1);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is BitArray64)
+            {
+                return (obj as BitArray64).num == num;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return (int)num;
+        }
+        //public static bool operator [](int number)
+        //{
+        //    return (num << iterator) & number;
+        //}
+        public static bool operator == (BitArray64 num1, object num2)
+        {
+            return num1.Equals(num2);
+        }
+        public static bool operator != (BitArray64 num1, object num2)
+        {
+            return !num1.Equals(num2);
+        }
+
+
+    }
+}
---
+++ Week7aWantsomeHomework/BitArray64/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray64 simpleArray = new BitArray64();
+            foreach(var i in simpleArray)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
