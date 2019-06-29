e804d8f9bc9164db3650dedf9d9ee862b51df31a
Humaniuc <humancoria@gmail.com>
2019-06-29 20:38:15 +0300

    Matrix<T>

---
+++ GenericsHomeworkA/GenericMatrix/Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GenericMatrix
+{
+    public class Matrix<T> where T : struct
+    {
+        public uint Rows { get; set; }
+        public uint Colls { get; set; }
+        public T[,] Matrixx { get; set; }
+        public Matrix()
+        {
+            Rows = ReadNatural("Write number of rows: ");
+            Colls = ReadNatural("Write numeber of colls: ");
+            Matrixx = new T[Rows, Colls];
+        }
+        public void Print()
+        {
+            for(int i = 0; i < Matrixx.GetLength(0); i++)
+            {
+                for(int j = 0; j < Matrixx.GetLength(1); j++)
+                {
+                    Console.Write($"{Matrixx[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static uint ReadNatural(string str)
+        {
+            uint elem;
+            while(true)
+            {
+                Console.WriteLine(str);
+                if(uint.TryParse(Console.ReadLine(), out elem))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Not a natural number!");
+                }
+            }
+            return elem;
+        }
+
+
+    }  
+}
---
+++ GenericsHomeworkA/GenericMatrix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix<int> matrix= new Matrix<int>();
+
+            matrix.Print();
+        }
+    }
+}
